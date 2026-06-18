a36e7976b48c374b130efdc2e5ae923f441fea73
diveevaaaa <diveevaaaa@users.noreply.github.com>
2026-06-18 10:46:12 +0000

    Add network movie loading
    Co-authored-by: diveevaaaa <diveevaaaa@users.noreply.github.com>

---
+++ MovieQuiz/Services/NetworkClient.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+enum NetworkError: Error {
+    case httpStatusCode(Int)
+    case urlRequestError(Error)
+    case urlSessionError
+    case apiError(String)
+    case imageDataConversionFailed
+}
+
+protocol NetworkRouting {
+    func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void)
+}
+
+final class NetworkClient: NetworkRouting {
+    private let session: URLSession
+
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void) {
+        let request = URLRequest(url: url)
+
+        session.dataTask(with: request) { data, response, error in
+            if let error {
+                handler(.failure(NetworkError.urlRequestError(error)))
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse else {
+                handler(.failure(NetworkError.urlSessionError))
+                return
+            }
+
+            guard 200..<300 ~= httpResponse.statusCode else {
+                handler(.failure(NetworkError.httpStatusCode(httpResponse.statusCode)))
+                return
+            }
+
+            guard let data else {
+                handler(.failure(NetworkError.urlSessionError))
+                return
+            }
+
+            handler(.success(data))
+        }.resume()
+    }
+}
--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -1,73 +1,78 @@
-import Foundation
+import UIKit
+
+protocol QuestionFactoryDelegate: AnyObject {
+    func didLoadDataFromServer()
+    func didFailToLoadData(with error: Error)
+    func didReceiveNextQuestion(question: QuizQuestion?)
+}
 
 protocol QuestionFactoryProtocol {
-    func requestNextQuestion() -> QuizQuestion?
+    var delegate: QuestionFactoryDelegate? { get set }
+
+    func loadData()
+    func requestNextQuestion()
     func reset()
 }
 
 final class QuestionFactory: QuestionFactoryProtocol {
+    weak var delegate: QuestionFactoryDelegate?
+
     private var currentQuestionIndex = 0
+    private var movies: [Movie] = []
+    private let moviesLoader: MoviesLoading
 
-    private let questions: [QuizQuestion] = [
-        QuizQuestion(
-            image: "The Godfather",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true
-        ),
-        QuizQuestion(
-            image: "The Dark Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true
-        ),
-        QuizQuestion(
-            image: "Kill Bill",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true
-        ),
-        QuizQuestion(
-            image: "The Avengers",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true
-        ),
-        QuizQuestion(
-            image: "Deadpool",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true
-        ),
-        QuizQuestion(
-            image: "The Green Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true
-        ),
-        QuizQuestion(
-            image: "Old",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false
-        ),
-        QuizQuestion(
-            image: "The Ice Age Adventures of Buck Wild",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false
-        ),
-        QuizQuestion(
-            image: "Tesla",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false
-        ),
-        QuizQuestion(
-            image: "Vivarium",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false
-        )
-    ]
+    init(moviesLoader: MoviesLoading = MoviesLoader()) {
+        self.moviesLoader = moviesLoader
+    }
 
-    func requestNextQuestion() -> QuizQuestion? {
-        let question = questions[safe: currentQuestionIndex]
+    func loadData() {
+        moviesLoader.loadMovies { [weak self] result in
+            guard let self else { return }
+
+            switch result {
+            case .success(let movies):
+                self.movies = movies.shuffled()
+                self.delegate?.didLoadDataFromServer()
+            case .failure(let error):
+                self.delegate?.didFailToLoadData(with: error)
+            }
+        }
+    }
+
+    func requestNextQuestion() {
+        guard let movie = movies[safe: currentQuestionIndex],
+              let rating = movie.rating,
+              let imageURL = movie.imageURL else {
+            delegate?.didReceiveNextQuestion(question: nil)
+            return
+        }
+
         currentQuestionIndex += 1
-        return question
+        moviesLoader.loadImageData(from: imageURL) { [weak self] result in
+            guard let self else { return }
+
+            switch result {
+            case .success(let data):
+                guard let image = UIImage(data: data) else {
+                    self.delegate?.didFailToLoadData(with: NetworkError.imageDataConversionFailed)
+                    return
+                }
+
+                let ratingThreshold = Int.random(in: 6...8)
+                let question = QuizQuestion(
+                    image: image,
+                    text: "Рейтинг этого фильма больше чем \(ratingThreshold)?",
+                    correctAnswer: rating > Float(ratingThreshold)
+                )
+                self.delegate?.didReceiveNextQuestion(question: question)
+            case .failure(let error):
+                self.delegate?.didFailToLoadData(with: error)
+            }
+        }
     }
 
     func reset() {
         currentQuestionIndex = 0
+        movies.shuffle()
     }
 }
